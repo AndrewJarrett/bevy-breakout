212fcd42cb45d5423cec58e8546bc3c54fe6836d
Andrew Jarrett <jarrett.andrew@gmail.com>
2024-01-20 22:26:57 -0500

    Add timer

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_plugins((DefaultPlugins, HelloPlugin))
         .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
+    }
 }
 
 #[derive(Component)]
@@ -13,6 +21,9 @@ struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Buzz Lightyear".to_string())));
@@ -24,9 +35,15 @@ fn hello_world() {
     println!("Hello world!");
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}!", name.0);
+fn greet_people(
+    time: Res<Time>,
+    mut timer: ResMut<GreetTimer>,
+    query: Query<&Name, With<Person>>
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }
     }
 }
 
