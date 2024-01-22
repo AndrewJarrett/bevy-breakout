29b728e74e60d5dfb0ab34ffa8484951e9c3ecf9
Andrew Jarrett <jarrett.andrew@gmail.com>
2024-01-22 00:49:58 -0500

    Add collision handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{
     prelude::*,
+    sprite::collide_aabb::{collide, Collision},
     sprite::MaterialMesh2dBundle
 };
 
@@ -19,8 +20,16 @@ pub struct BreakoutPlugin;
 
 impl Plugin for BreakoutPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
-            .add_systems(FixedUpdate, apply_velocity)
+        app
+            .add_event::<CollisionEvent>()
+            .add_systems(Startup, setup)
+            .add_systems(
+                FixedUpdate,
+                (
+                    apply_velocity,
+                    check_collisions
+                ).chain()
+            )
             .add_systems(Update, bevy::window::close_on_esc);
     }
 }
@@ -30,6 +39,12 @@ struct Ball;
 
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Collider;
+
+#[derive(Event, Default)]
+struct CollisionEvent;
 
 fn setup(
     mut commands: Commands,
@@ -59,6 +74,52 @@ fn apply_velocity(
         transform.translation.x += velocity.x * time.delta_seconds();
         transform.translation.y += velocity.y * time.delta_seconds();
     }
+}
+
+fn check_collisions(
+    mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
+    collider_query: Query<(Entity, &Transform), With<Collider>>,
+    mut collision_events: EventWriter<CollisionEvent>,
+) {
+    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
+    let ball_size = ball_transform.scale.truncate();
+
+    // Check for a collision
+    for (_collider_entity, transform) in &collider_query {
+        let collision = collide(
+            ball_transform.translation,
+            ball_size,
+            transform.translation,
+            transform.scale.truncate(),
+        );
+
+        if let Some(collision) = collision {
+            // Send a collision event so other systems can react to it
+            collision_events.send_default();
+
+            // Reflect the ball when it collides
+            let mut reflect_x = false;
+            let mut reflect_y = false;
+
+            // Only reflect if the Ball's velocity is going in the opposite direction
+            match collision {
+                Collision::Left => reflect_x = ball_velocity.x > 0.0,
+                Collision::Right => reflect_x = ball_velocity.x < 0.0,
+                Collision::Top => reflect_y = ball_velocity.y < 0.0,
+                Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
+                Collision::Inside => {}
+            }
+
+            if reflect_x {
+                ball_velocity.x = -ball_velocity.x;
+            }
+
+            if reflect_y {
+                ball_velocity.y = -ball_velocity.y;
+            }
+        }
+    }
+
 }
 
 /*
